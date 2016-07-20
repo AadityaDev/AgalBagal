9ef73774c384236a8cde4f41f7d6ef66b8cb13f4
AadityaDev <adityaverma.dev@gmail.com>
2016-07-20 12:58:23 +0530

    Project setup

---
+++ app/src/main/java/com/technawabs/agalbagal/receivers/InstanceIDService.java
@@ -0,0 +1,25 @@
+package com.technawabs.agalbagal.receivers;
+
+import android.content.res.Resources;
+import android.content.res.Resources.Theme;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class InstanceIDService extends FirebaseInstanceIdService{
+    private final String TAG=this.getClass().getSimpleName();
+
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // TODO: Implement this method to send any registration to your app's servers.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String token) {
+        // Add custom implementation, as needed.
+    }
+}
